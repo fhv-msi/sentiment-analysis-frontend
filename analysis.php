6384438a7b87e56a5be0241d37ecaf86c5a82f63
Michael Sieber <msieber88@gmail.com>
2017-11-19 09:37:25 +0100

    fix logout redirect

--- analysis.php
+++ analysis.php
@@ -106,7 +106,7 @@
                     <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                     <div class="modal-footer">
                         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="index.html" id="logoutBtn">Logout</a>
+                        <a class="btn btn-primary" href="index.php" id="logoutBtn">Logout</a>
                     </div>
                 </div>
             </div>
