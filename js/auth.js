365bc9403aedf0f8b7b3a7486a305165cf2f320e
Michael Sieber <msieber88@gmail.com>
2017-11-21 19:32:36 +0100

    fix logout redirect url

--- js/auth.js
+++ js/auth.js
@@ -31,7 +31,7 @@ $(function() {
             },
             success: function (response) {
                 clear();
-                window.location.replace("index.html");
+                window.location.replace("index.php");
             },
             error: function (response) {
                 alert('error during login, please try again');
